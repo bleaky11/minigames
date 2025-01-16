76b3df06a8c7ce739dc0d7e32554b0a8d41399ea
bleaky11 <levichen533@gmail.com>
2025-01-16 16:31:59 -0500

    can now assign each square x or o(not displayed yet)

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,34 @@
+'use client'
+
+import { useState } from "react";
+
+type player = 'x' | 'o'
+
 export default function Home() {
-  
+  const [spots, setSpots] = useState<string>('#########')
+  const [player, setPlayer] = useState<player>('x')
+
+  function setSpotHelper(num: number): void {
+    const newSpots = spots.slice(0, num -1) + (player as string) + spots.slice(num)
+    setSpots(newSpots)
+    if(player == 'x') setPlayer('o');
+    else setPlayer('x');
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 style={{font:'menu'}}>Hello this page will be blank for now while I work on it</h1>
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 style={{font:'menu'}}>Ignore the weird color</h1>
+      <h2> Current spots are: {spots} </h2>
       <div className="square">
-        <div className="smaller_square"></div>
-        <div className="smaller_square"></div>
-        <div className="smaller_square"></div>
-        <div className="smaller_square"></div>
-        <div className="smaller_square"></div>
-        <div className="smaller_square"></div>
-        <div className="smaller_square"></div>
-        <div className="smaller_square"></div>
-        <div className="smaller_square"></div>
+        <div className="smaller_square" onClick={() => setSpotHelper(1)}></div>
+        <div className="smaller_square" onClick={() => setSpotHelper(2)}></div>
+        <div className="smaller_square" onClick={() => setSpotHelper(3)}></div>
+        <div className="smaller_square" onClick={() => setSpotHelper(4)}></div>
+        <div className="smaller_square" onClick={() => setSpotHelper(5)}></div>
+        <div className="smaller_square" onClick={() => setSpotHelper(6)}></div>
+        <div className="smaller_square" onClick={() => setSpotHelper(7)}></div>
+        <div className="smaller_square" onClick={() => setSpotHelper(8)}></div>
+        <div className="smaller_square" onClick={() => setSpotHelper(9)}></div>
       </div>
     </div>
   );
