f3e602b538436fc9a7f72021a065a98909a2a6fe
bleaky11 <levichen533@gmail.com>
2025-01-31 18:28:50 -0500

    Game now displays winner and prevents moving after someone wins

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from 'next/image';
 import Circle from "./Images/LetterO.png"
 import Cross from "./Images/LetterX.png"
@@ -9,19 +9,42 @@ type player = 'x' | 'o'
 export default function Home() {
   const [spots, setSpots] = useState<string>('#########')
   const [player, setPlayer] = useState<player>('x')
+  const [winner, setWinner] = useState<string>()
 
   function setSpotHelper(num: number): void {
     const newSpots = spots.slice(0, num -1) + (player as string) + spots.slice(num)
-    if(spots[num - 1] === '#')
+    if(spots[num - 1] === '#' && !winner){
       setSpots(newSpots)
-    if(player == 'x') setPlayer('o');
-    else setPlayer('x');
+      if(player == 'x') setPlayer('o');
+      else setPlayer('x');
+    }
+    
+  }
+
+  useEffect(() => {
+    checkWinner();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [spots]);
+
+  function checkWinner(): void {
+    if((spots[0] === spots[1] && spots[1] === spots[2] )||
+    (spots[0] === spots[3] && spots[3] === spots[6]) ||
+    (spots[0] === spots[4] && spots[4] === spots[8])){
+      if(spots[0] !== '#') setWinner(spots[0])}
+    else if((spots[8] === spots[7] && spots[7] === spots[6]) || 
+    (spots[8] === spots[5] && spots[5] === spots[2])){
+      if(spots[8] !== '#') setWinner(spots[8])}
+    else if((spots[4] === spots[2] && spots[4] === spots[6]) ||
+    (spots[4] === spots[3] && spots[4] === spots[5]) ||
+    (spots[4] === spots[1] && spots[4] === spots[7])){
+      if(spots[4] !== '#') setWinner(spots[4])}
+    
   }
 
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 style={{font:'menu'}}>background in progress</h1>
-      <h2> Current spots are: {spots} </h2>
+      <h2> Current Player is: {player}</h2>
+      {(winner) && <h2>The Winner is: {winner}</h2>}
       <div className="square">
         <div className="smaller_square" onClick={() => setSpotHelper(1)}>
           {spots[0] == 'o' && (<Image src={Circle} alt="circle here" width='166' height="166"/>)}
